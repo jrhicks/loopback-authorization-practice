5829232d44bab95aebc4641081a05488be11b542
Jeffrey <jrhicks@cteh.com>
2016-02-07 09:57:35 -0600

    looks like we can access the userId in a hook

--- common/models/siera.js
+++ common/models/siera.js
@@ -1,11 +1,18 @@
+var loopback = require('loopback');
+
 module.exports = function(Siera) {
 
   Siera.observe('access', function limitToPublished(ctx, next) {
+
+    var loopbackCtx = loopback.getCurrentContext();
+    var userId = loopbackCtx.active.accessToken.userId;
+
     if (ctx.query.where === undefined) {
       ctx.query = {"where": {"status":"published"}}
     } else {
       ctx.query.where.status = "published"
     }
+
     next();
   });
 
