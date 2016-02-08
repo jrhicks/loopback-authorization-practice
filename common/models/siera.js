fc97908a335969e043b713a44b246bacac428d7a
Jeffrey <jrhicks@cteh.com>
2016-02-07 23:32:36 -0600

    refactered filters

--- common/models/siera.js
+++ common/models/siera.js
@@ -1,19 +1,10 @@
 var loopback = require('loopback');
-var assert = require('assert');
-var addWhereFilter = require('../../server/lib/addWhereFilter');
+var applyRoleFilters = require('../../lib/apply-role-filters');
 
 module.exports = function(Siera) {
 
-  Siera.observe('access', function applyRoleFilters(ctx, next) {
-    var role_mapping = Siera.app.models.RoleMapping;
-    var userId = loopback.getCurrentContext().active.accessToken.userId;
-    assert(userId);
-    role_mapping.getRoleNames(userId, function(roles) {
-      if (roles.indexOf('client') > -1) {
-        ctx = addWhereFilter(ctx, {"status":"published"});
-      }
-      next();
-    });
-  });
+  Siera.observe('access', applyRoleFilters(Siera,
+    {client: {status: 'published'}}
+  ));
 
 };
